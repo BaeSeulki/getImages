c0fe310cfbb50f4c9ee44c5468f05b5435fcc10a
Chill Yu <zhouyu617@vip.qq.com>
2018-01-08 08:14:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,22 @@ import os
 import datetime
 
 
-def run(flag):
+def run(flag, k):
     j = 0
     # 获取可用的IP代理，保存在proxy.txt中
     print("获取可用的IP代理...")
     os.system("python3 getImages/fetch_free_proxies.py")
     while True:
-        # 每90秒执行一次爬虫
         if flag:
-            i = 90
+            i = k
             j = j + i
         else:
-            i = 3600
+            i = 7200
             j = i
         os.system("scrapy crawl oinbag")
         print("waiting......当前更新频率为%s秒" % i)
         time.sleep(i)
-        if j == 3600:
+        if j >= 3600:
             # 每小时更新一次IP代理列表
             os.system("python3 getImages/fetch_free_proxies.py")
             j = 0
@@ -31,12 +30,13 @@ def night():
     time = now.strftime('%Y-%m-%d %H:%M:%S')
     hour = time.strip().split(' ')[1].split(':')[0]
     # print(int(hour))
-    if int(hour) < 7:
+    if int(hour) < 8:
         return False
     else:
         return True
 
 
 if __name__ == '__main__':
+    k = 90   # 设置更新频率s
     a = night()
-    run(a)
+    run(a, k)
